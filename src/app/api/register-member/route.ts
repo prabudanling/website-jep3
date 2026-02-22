c78dccbbc3fccae8d356cda1633e9d15396fa1c6
prabudanling <prabudanling@gmail.com>
2026-02-22 08:30:18 +0700

    fix:update

--- src/app/api/register-member/route.ts
+++ src/app/api/register-member/route.ts
@@ -1,4 +1,3 @@
-// app/api/register-member/route.ts
 import { NextResponse } from 'next/server'
 import { supabase } from '@/lib/supabaseClient'
 
--- src/app/daftar-member/page.tsx
+++ src/app/daftar-member/page.tsx
@@ -1,4 +1,3 @@
-// app/daftar-member/page.tsx
 'use client'
 
 import { useEffect, useState } from 'react'
